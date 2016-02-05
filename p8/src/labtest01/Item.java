460ea5651f87cfd8019aeda752f1635370842d22
rvladi1 <rvladi1@lab2-39.CS.McGill.CA>
2016-02-05 09:15:32 -0500

    added PriceComparator to Item

--- p8/src/labtest01/Item.java
+++ p8/src/labtest01/Item.java
@@ -1,50 +1,67 @@
 package labtest01;
 
+import java.util.Comparator;
+
 /**
- * An item in a grocery store: 
- * Jar of peanut butter, package of pasta,
- * whatever.
+ * An item in a grocery store: Jar of peanut butter, package of pasta, whatever.
  */
-public class Item
-{
+public class Item {
+
+	private static final Comparator<Item> PRICE_COMPARATOR = new PriceComparator();
+
 	private final String aName;
 	private final int aId;
 	private final int aPrice; // In cents: 100 = one dollar
-	
+
 	/**
 	 * Creates a new item.
-	 * @param pName The name of the item. Not necessarily unique.
-	 * @param pId A unique id for the item
-	 * @param pPrice The price of the item in cents
+	 * 
+	 * @param pName
+	 *            The name of the item. Not necessarily unique.
+	 * @param pId
+	 *            A unique id for the item
+	 * @param pPrice
+	 *            The price of the item in cents
 	 */
-	public Item(String pName, int pId, int pPrice)
-	{
+	public Item(String pName, int pId, int pPrice) {
 		aName = pName;
 		aId = pId;
 		aPrice = pPrice;
 	}
-	
+
 	/**
 	 * @return The name of the item
 	 */
-	public String getName()
-	{
+	public String getName() {
 		return aName;
 	}
-	
+
 	/**
 	 * @return The ID of the item.
 	 */
-	public int getId()
-	{
+	public int getId() {
 		return aId;
 	}
-	
+
 	/**
 	 * @return The price of the item in cents
 	 */
-	public int getPrice()
-	{
+	public int getPrice() {
 		return aPrice;
+	}
+
+	public static Comparator<Item> getPriceComparator() {
+		return PRICE_COMPARATOR;
+	}
+
+	private static class PriceComparator implements Comparator<Item> {
+		/**
+		 * @pre pItem1 != null && pItem2 != null
+		 */
+		@Override
+		public int compare(Item pItem1, Item pItem2) {
+			assert pItem1 != null & pItem2 != null;
+			return pItem1.aPrice - pItem2.aPrice;
+		}
 	}
 }
